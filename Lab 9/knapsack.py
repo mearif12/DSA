8129ef8956529f958d3a6690f870a01406b39d3d
ME Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2025-02-24 23:25:45 +0600

    0/1 knapsack algorithm using python

--- Lab 9/knapsack.py
+++ Lab 9/knapsack.py
@@ -46,3 +46,34 @@ def knapsack():
 
 if __name__ == "__main__":
     knapsack()
+    
+#     def knapsack():
+#     n = int(input("Enter number of items: "))
+#     profits = [int(input(f"Profit of item {i+1}: ")) for i in range(n)]
+#     weights = [int(input(f"Weight of item {i+1}: ")) for i in range(n)]
+#     capacity = int(input("Enter knapsack capacity: "))
+    
+#     dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+    
+#     for i in range(1, n + 1):
+#         for w in range(1, capacity + 1):
+#             if weights[i - 1] <= w:
+#                 dp[i][w] = max(profits[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+#             else:
+#                 dp[i][w] = dp[i - 1][w]
+    
+#     print("\nDP Table:")
+#     for row in dp:
+#         print(" ".join(f"{x:2d}" for x in row))
+    
+#     print("\nMaximum profit:", dp[n][capacity])
+    
+#     w = capacity
+#     print("Items included:")
+#     for i in range(n, 0, -1):
+#         if dp[i][w] != dp[i - 1][w]:
+#             print(f"Item {i} (Profit: {profits[i-1]}, Weight: {weights[i-1]})")
+#             w -= weights[i - 1]
+
+# if __name__ == "__main__":
+#     knapsack()
