9395be4baf2745bcebd941b1fa490b56b95fab75
ME Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2025-02-26 13:05:34 +0600

    tower of hanoi using python

--- Lab 8/tower.py
+++ Lab 8/tower.py
@@ -1,13 +1,15 @@
-def hanoi(n, source, target, auxiliary):
+def hanoi(n, source, target, auxiliary, count):
     if n == 1:
-        print(f"Move disk 1 from {source} to {target}")
+        count[0] += 1  
+        print(f"{count[0]}: Move disk 1 from {source} to {target}")
         return
-    hanoi(n - 1, source, auxiliary, target)  # Move n-1 disks to auxiliary peg
-    print(f"Move disk {n} from {source} to {target}")  # Move largest disk to target
-    hanoi(n - 1, auxiliary, target, source)  # Move n-1 disks from auxiliary to target
+    hanoi(n - 1, source, auxiliary, target, count) 
+    count[0] += 1
+    print(f"{count[0]}: Move disk {n} from {source} to {target}")  
+    hanoi(n - 1, auxiliary, target, source, count)  
 
-# Main function
 n = int(input("Enter number of disks: "))
 print(f"Total moves required: {2**n - 1}")
-print(f"The sequence of moves involved in the Tower of Hanoi are:")
-hanoi(n, 'A', 'C', 'B')  # Call function with pegs A (source), C (target), B (auxiliary)
+print("The sequence of moves involved in the Tower of Hanoi are:")
+count = [0] 
+hanoi(n, 'A', 'C', 'B', count)
